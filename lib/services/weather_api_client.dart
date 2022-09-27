73ecaf36729152197ab763792a622a3154ce1da3
Wbaidur Rahman <wbaidurrahman2000@gmail.com>
2022-09-27 23:17:23 +0600

    Calling Api from the app is completed

--- lib/services/weather_api_client.dart
+++ lib/services/weather_api_client.dart
@@ -6,7 +6,7 @@ import 'package:weatherapp/model/weather_model.dart';
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather(String? location) async {
     var endpoint = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=2f527abaf5426ef89d9044134bd36003"
+        "https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid={api_key}"
     );
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
